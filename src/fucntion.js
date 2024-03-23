365d824acb954eec1977138ef2fe6c2c10968df2
hrmarquezchn <hrmarquez69@gmail.com>
2024-03-23 07:28:21 -0600

    03

--- src/fucntion.js
+++ src/fucntion.js
@@ -1,6 +1,7 @@
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 export function alerta (mensaje, icono, id=''){
     onFocus(id);
